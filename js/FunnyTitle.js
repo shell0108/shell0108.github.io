503f48a8799303bf3f1a608e14a617b10dcf3f1d
贾黄春 <22581jhc@gmail.com>
2020-07-10 11:32:38 +0800

    Site updated: 2020-07-10 11:32:38

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "../img/funny.png");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/xingjiahui/CDN@latest/2020/07/10/afeaf40722541a5c1636c85718694608.png");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "../img/favicon.png");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/xingjiahui/CDN@latest/2020/07/10/a78a32d61c8ad4ec7c698dfd0b0e4f14.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
